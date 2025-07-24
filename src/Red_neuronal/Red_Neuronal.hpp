2ca1653d4e5b18706a777cf25dc4f0db5c389083
ryther <rodrigo.palacios.salas@ucsp.edu.pe>
2025-07-24 10:40:25 -0500

    red_neuronal_temrinada

--- src/Red_neuronal/Red_Neuronal.hpp
+++ src/Red_neuronal/Red_Neuronal.hpp
@@ -23,15 +23,3 @@ class Red_Neuronal{
 
 
 
-
-// en el codigo implementar la libreria cmath, vector(solo si es nesesario),random(para los pesos)
-//ademas de eso:
-/*
-    acabar las capas y que se unan en la clase red neuronal para formarla 
-    cambiar los datos de Expo aprox a la libreria c++
-    terminar la red neuronal 
-
-        si hay tiempo
-            hacer para que los datos de los vectores se devuelvan y se ingresen por TXTs
-            asi que los datos se ingresen por TXTs y se guarden por TXTs
-*/
